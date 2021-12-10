cf4db4f0be43ed09253defa2620d30d3260bde38
itorest <78473381+itorest@users.noreply.github.com>
2021-12-10 21:42:06 +0800

    test: testing csp

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const securityHeaders = [
   {
     key: 'Content-Security-Policy',
-    value: `default-src 'self'`,
+    value: `default-src 'self';script-src 'self' 'unsafe-eval' 'unsafe-inline';style-src 'self' 'unsafe-inline'`,
   },
 ];
 
